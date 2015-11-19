453771163b29417cd5cb828c9ab6b4b652b5220e
iriver <classicalsupport_mrrivernews@hotmail.com>
2015-11-19 18:32:12 +0800

    test commit

---
+++ greensock_playground/play.js
@@ -0,0 +1,12 @@
+// Version Control
+// use this repo: https://github.com/jean61727/scad-final.git
+$(function(){
+  TweenLite.fromTo(
+    '#banner',5,{
+                   backgroundColor:'red'
+                   },{
+                   backgroundColor:'blue'
+                   }
+  );
+  
+});
