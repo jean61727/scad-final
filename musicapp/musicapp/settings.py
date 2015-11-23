32fde855ae2382bc5acc6636dd047b76db365fe7
jean <jean61727@hotmail.com>
2015-11-23 13:32:56 +0800

    seetings from mysql to sqlite

--- musicapp/musicapp/settings.py
+++ musicapp/musicapp/settings.py
@@ -59,14 +59,14 @@ WSGI_APPLICATION = 'musicapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE':'django.db.backends.mysql',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'musicproject',
-        'USER': 'musicprojectuser',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE':'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': 'musicproject',
+        #'USER': 'musicprojectuser',
+        #'PASSWORD': 'password',
+        #'HOST': 'localhost',
+        #'PORT': '',
     }
 }
 
