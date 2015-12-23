dcb051c3b0f69e15c455a1bc69650d3400be1d45
iriver <classicalsupport_mrrivernews@hotmail.com>
2015-12-23 14:21:39 +0800

    map root url to home tab

--- musicapp/musicapp/urls.py
+++ musicapp/musicapp/urls.py
@@ -8,18 +8,19 @@ urlpatterns = patterns('',
     # url(r'^$', 'musicapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # Main View Tab Content
+    url(r'^$', home_view, name='home'),
+    url(r'^home/$', home_view, name='home'),
+    url(r'^explore/songcategories/$',exploreSongCategories),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', logout_page),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
     url(r'^register/$', register),
     url(r'^register/success/$', register_success),
-    url(r'^home/$', home_view, name='home'),
     url(r'^app/$', base,name = 'mainpage'),
     url(r'^login_home/$',home_login),
     url(r'^post/$',user_post),
-    url(r'^explore/songcategories/$',exploreSongCategories),
-
-
 
 )
