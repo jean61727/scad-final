3344a591e3028c5bd11bc238c333f50864d2c355
Christine Lu <christine6656@yahoo.com.tw>
2015-12-24 22:05:09 +0800

    ?

--- musicapp/posts/models.py
+++ musicapp/posts/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     likes = models.IntegerField(null=True)
     people_listening = models.IntegerField(null=True)
     user_id = models.ForeignKey('login.CustomUser', on_delete=models.CASCADE,null=True)
+    user_pic_path = models.CharField(max_length=200, null=True);
     category = models.CharField(max_length=20,null=True)
 
     time = models.DateTimeField(default=datetime.now)
@@ -27,5 +28,6 @@ class Comments(models.Model):
     # comment_id = models.CharField(max_length=50)
     # commented user id or username? should be discussed
     # comment_user_id = models.CharField(max_length=50)
+    post_id = models.CharField(max_length=100, null=True)
     comment_message = models.CharField(max_length=300)
     time = models.DateTimeField(auto_now_add=True)
