a5f14115c9caec78560c93ab8e39cbed8bc3f58c
iriver <classicalsupport_mrrivernews@hotmail.com>
2015-12-29 09:15:14 +0800

    modulize home tab post

---
+++ musicapp/musicSite/static/js/site_utilities.js
@@ -0,0 +1,7 @@
+function ajax_fail_handler(xhr, textStatus, errorThrown){
+	console.log("ajax failed. Showing error message:");
+	// simply pass the following into console.log():
+	// use xhr.responseText to print response message
+	// use textStatus and error to see server error message
+	console.log(xhr.responseText);
+}
