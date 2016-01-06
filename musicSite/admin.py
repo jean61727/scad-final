0f97dc8a64ef8f578fc8ba74ae4dca908305bd84
iriver <classicalsupport_mrrivernews@hotmail.com>
2016-01-06 15:03:23 +0800

    add sufficient field to admin db view

--- musicSite/admin.py
+++ musicSite/admin.py
@@ -1,16 +1,28 @@
 from django.contrib import admin
 
 from posts.models import *
+from login.models import CustomUser
+
 # Register your models here.
 class PostViewAdmin(admin.ModelAdmin):
-	list_display = ('post_message', 'user_id_id', 'time')
+	list_display = ('id','title', 'user_id_id', 'likes','post_message','category', 'url','start_time','people_listening', 'time')
+admin.site.register(Post, PostViewAdmin)
 
 class CommentViewAdmin(admin.ModelAdmin):
-	list_display = ('comment_message', 'user_id', 'post_id','time')
-
-
-admin.site.register(Post, PostViewAdmin)
+	# all
+	list_display = ('id','user_id', 'comment_message' , 'post_id','time')
 admin.site.register(Comment, CommentViewAdmin)
 
+class UserViewAdmin(admin.ModelAdmin):
+	list_display = ('id','username','user_image')
+admin.site.register(CustomUser, UserViewAdmin)
+
+class  FollowerViewAdmin(admin.ModelAdmin):
+	"""docstring for  FollowerViewAdmin"""
+	list_display = ('id','user_id_id', 'follow',)
 admin.site.register(Follower)
+
+class LikeViewAdmin(admin.ModelAdmin):
+	"""docstring for LikeViewAdmin"""
+	list_display = ('id', 'post_id', 'user_id')
 admin.site.register(Like)
