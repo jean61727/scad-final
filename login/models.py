007adbbfdc6df033a490afe8c42df60a77a7258b
iriver <classicalsupport_mrrivernews@hotmail.com>
2016-01-07 01:49:03 +0800

    like mechanism finished

--- login/models.py
+++ login/models.py
@@ -1,9 +1,9 @@
 ﻿from django.db import models
 from django.contrib.auth.models import User, UserManager
 # Create your models here.
-#class MyUser(models.Model):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#    user_image = models.TextField(max_length=100)
+###class MyUser(models.Model):
+####   user = models.OneToOneField(User, on_delete=models.CASCADE)
+####   user_image = models.TextField(max_length=100)
 
 class CustomUser(User):
 	user_image = models.TextField(max_length=100)
