147206e419b798e62380c9bd45ccd0aac3e4a3a4
iriver <classicalsupport_mrrivernews@hotmail.com>
2016-01-08 01:00:17 +0800

    damn we can't get rid of mouser

--- login/models.py
+++ login/models.py
@@ -2,9 +2,9 @@
 from django.contrib.auth.models import User, UserManager
 # Create your models here.
 
-#class MyUser(models.Model):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#    user_image = models.TextField(max_length=100)
+class MyUser(models.Model):
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   user_image = models.TextField(max_length=100)
 
 
 class CustomUser(User):
