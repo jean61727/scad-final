4d63ec3cfa6cdd56ef22882f1348592036aace63
iriver <classicalsupport_mrrivernews@hotmail.com>
2016-01-08 00:34:26 +0800

    hotfix

--- login/models.py
+++ login/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User, UserManager
 # Create your models here.
 
 #class MyUser(models.Model):
-#        user = models.OneToOneField(User, on_delete=models.CASCADE)
-#        user_image = models.TextField(max_length=100)
+#    user = models.OneToOneField(User, on_delete=models.CASCADE)
+#    user_image = models.TextField(max_length=100)
 
 
 class CustomUser(User):
