9a6384a1eecab9b80eb2fd4403d2f8e71674f1a3
iriver <classicalsupport_mrrivernews@hotmail.com>
2016-01-14 17:13:34 +0800

    commented customuser model

--- login/models.py
+++ login/models.py
@@ -7,6 +7,6 @@ from django.contrib.auth.models import User, UserManager
 #    user_image = models.TextField(max_length=100)
 
 
-class CustomUser(User):
-	user_image = models.TextField(max_length=100)
-	objects = UserManager()
+# class CustomUser(User):
+# 	user_image = models.TextField(max_length=100)
+# 	objects = UserManager()
