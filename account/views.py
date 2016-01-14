87bd556e223f76fefb050cb7f3ca262616a3767f
iriver <classicalsupport_mrrivernews@hotmail.com>
2016-01-14 23:15:46 +0800

    Finally deployed on heroku. Watch out any db related error.

--- account/views.py
+++ account/views.py
@@ -34,7 +34,7 @@ def db_account_image(request):
 		json_data = json.loads(request.body.decode('utf-8'))
 		if json_data["request_type"] == "update_profile_image":
 			# print "now the user is",request.user
-			CustomUser.objects.filter(user_id__username=request.user).update(user_image=json_data["image_path"])
+			CustomUser.objects.filter(username=request.user).update(user_image=json_data["image_path"])
 			return HttpResponse("haha updated the db")
 		elif json_data["request_type"] == "get_profile_image":
 			pass
