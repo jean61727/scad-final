6dd5b36bff8d277cd2f263bc0e8cecf1faac6fbd
Shaung Cheng <shaungcheng@0587364432.wireless.umich.net>
2018-02-05 16:24:55 -0500

    resolving staticfile issue

--- musicapp/production_settings.py
+++ musicapp/production_settings.py
@@ -8,8 +8,11 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
+import os
 # Static asset configuration.
 # STATIC_ROOT = 'staticfiles'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
