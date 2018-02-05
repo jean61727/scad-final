65eeed8ac989a84962013f3c2c3eae3c02a9e7a5
Shaung Cheng <shaungcheng@0587364432.wireless.umich.net>
2018-02-05 16:20:21 -0500

    exclude/commented out STATIC_ROOT in production_setting

--- musicapp/production_settings.py
+++ musicapp/production_settings.py
@@ -9,7 +9,7 @@ DATABASES = {
 }
 
 # Static asset configuration.
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
