8e962593c24354e15cbd4e39ccdd5fe5c1cd314b
Shaung Cheng <shaungcheng@Shaungde-MacBook-Air.local>
2018-02-06 00:12:03 -0500

    fix

--- musicapp/settings.py
+++ musicapp/settings.py
@@ -66,6 +66,8 @@ DATABASES = {
 
 try:
     print('maybe on heroku? url is', os.environ['DATABASE_URL'])
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
     DEBUG = True
     # ALLOWED_HOSTS = ['*']
     # Update database configuration with $DATABASE_URL. 
